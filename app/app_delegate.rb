142631cda139289d527de926429dd9b48f7978d7
Nick Pachulski <hello@nickpachulski.com>
2013-06-01 01:34:15 -0700

    Fixed UITableViewCell constraints issue.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -7,61 +7,48 @@ class AppDelegate
 end
 
 class TableViewController < UITableViewController
+  stylesheet :main
+
   def viewDidLoad
-    self.view.registerClass(CustomCell, 
-      forCellReuseIdentifier:'cell id')
+    self.view.registerClass(CustomCell, forCellReuseIdentifier:'cell id')
   end
-  
+
   def tableView(tableView, numberOfSectionsInTableView:section)
     1
   end
-  
+
   def tableView(tableView, numberOfRowsInSection:section)
     3
   end
-  
+
+  def tableView(tableView, heightForRowAtIndexPath:index_path)
+    100
+  end
+
   def tableView(tableView, cellForRowAtIndexPath:indexPath)
     cell = tableView.dequeueReusableCellWithIdentifier('cell id')
-    
-    cell.title_label = "title #{indexPath.row}"
-    cell.details_label = "details #{indexPath.row}"
-    cell.attendees_label = "attendees #{indexPath.row}"
-    
+    cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton
+
+    layout(cell.contentView, :root) do
+      subview(UIView, :padding) do
+        cell.title_label = subview(UILabel, :cell_title_label, :text => "title #{indexPath.row}")
+        cell.details_label = subview(UILabel, :cell_details_label, :text => "details #{indexPath.row}")
+        cell.attendees_label = subview(UILabel, :cell_attendees_label, :text => "attendees #{indexPath.row}")
+      end
+    end
+    cell.contentView.apply_constraints
     cell
   end
 end
 
 class CustomCell < UITableViewCell
   attr_accessor :title_label, :details_label, :attendees_label
-  # include Teacup::Layout
-  stylesheet :main
-
-  def initWithStyle(style, reuseIdentifier:cell_identifier)
-    super
-    create_cell_subviews
-    self
-  end
-
-  def create_cell_subviews
-    self.accessoryType = UITableViewCellAccessoryDetailDisclosureButton
-
-    layout(self.contentView, :root) do
-      subview UIView, :padding do
-        self.title_label = subview UILabel, :cell_title_label
-        self.details_label = subview UILabel, :cell_details_label
-        self.attendees_label = subview UILabel, :cell_attendees_label
-      end
-      self.contentView.apply_constraints
-    end
-
-    self
-  end
 end
 
 Teacup::Stylesheet.new :main do
   style :root,
     backgroundColor: UIColor.colorWithRed(247.0 / 256.0, green:221.0 / 256.0, blue:186.0 / 256.0, alpha:1)
-  
+
   style :padding,
    backgroundColor: UIColor.greenColor,
    constraints: [
